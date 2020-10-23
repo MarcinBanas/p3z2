69c7547b8a0dcba4a1604976b774672b9d108769
Marcin Banaś <mb056625@student.ath.edu.pl>
2020-10-23 18:45:19 +0200

    Aplikacja wyznacza miejsca zerowe

--- p3z2/Program.cs
+++ p3z2/Program.cs
@@ -15,15 +15,15 @@ namespace p3z2
             string odpowiedz = Console.ReadLine();
             int a;
             bool CzySieUdaloA = int.TryParse(odpowiedz, out a);
-            while(CzySieUdaloA==false)
+            while (!CzySieUdaloA)
             {
-                    Console.WriteLine("Zle dane");
-                    odpowiedz = Console.ReadLine();
-                    CzySieUdaloA = int.TryParse(odpowiedz, out a);
+                Console.WriteLine("Zle dane");
+                odpowiedz = Console.ReadLine();
+                CzySieUdaloA = int.TryParse(odpowiedz, out a);
             }
-                return a;
+            return a;
         }
-        
+
         static double Delta(double a, double b, double c)
         {
             double delta = (b * b) - 4 * a * c;
@@ -31,19 +31,21 @@ namespace p3z2
         }
         static void Main(string[] args)
         {
+
             Console.WriteLine("Podaj wspolczynnik a:");
             double a = Wczytaj();
             Console.WriteLine("Podaj wspolczynnik b:");
             double b = Wczytaj();
             Console.WriteLine("Podaj wspolczynnik c:");
             double c = Wczytaj();
-            Console.WriteLine("delta:" + Delta(a, b, c));
+            double WyliczonaDelta = Delta(a, b, c);
+            Console.WriteLine("delta:" + WyliczonaDelta);
             Wyniki MZ;
-            if (Delta(a, b, c) < 0)
+            if (WyliczonaDelta < 0)
             {
                 MZ = Wyniki.BrakMiejscZerowych;
             }
-            else if (Delta(a, b, c) == 0)
+            else if (WyliczonaDelta == 0)
             {
                 MZ = Wyniki.JednoMiejsceZerowe;
             }
@@ -57,10 +59,14 @@ namespace p3z2
                     Console.WriteLine("Funkcja nie posiada miejsc zerowych.");
                     break;
                 case Wyniki.JednoMiejsceZerowe:
-                    Console.WriteLine("Funkcja posiada jedno miejsce zerowe.");
+                    double x1 = ((-b) / (2 * a));
+                    Console.WriteLine("Funkcja posiada jedno miejsce zerowe:" + x1);
                     break;
                 case Wyniki.DwaMiejscaZerowe:
-                    Console.WriteLine("Funkcja posiada dwa miejsca zerowe.");
+                    double x2 = ((-b - WyliczonaDelta) / (2 * a));
+                    double x3 = ((-b + WyliczonaDelta) / (2 * a));
+                    Console.Write("Funkcja posiada dwa miejsca zerowe x1:" + x2);
+                    Console.WriteLine(" x2:" + x3);
                     break;
                 default:
                     Console.WriteLine("Brak Wynikow.");
